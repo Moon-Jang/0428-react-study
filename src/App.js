1c7c4569c443778deee57a4c337fe82196949330
Moon-Jang <roby8502@naver.com>
2021-04-28 17:38:14 +0900

    setting study

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
-
+import './styles/App.css';
+//import Header from "./components/Header";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+		<header>
+			<div className="header-wrap">
+				<div className="title-logo"></div>
+			</div>
+		</header>
+		<div className="contents-wrap">
+			<div className="article-wrap">
+				<div className="writer-wrap">
+					<div className="writer-image"></div>
+					<div className="writer-name">
+						<p className="name">elizabeth gilmore</p>
+					</div>
+				</div>
+				<div className="photo-wrap">
+					<div className="photo"></div>
+				</div>
+				<div className="button-wrap">
+					<input type="checkbox" id="likeCheck" />
+					<label for="likeCheck"></label>
+					<input type="checkbox" id="commentCheck" />
+					<label for="commentCheck"></label>
+				</div>
+				<div className="description-wrap">
+					<p className="description"><b>elzabeth glimore:</b> 우리 우정 영원히...</p>
+				</div>
+			</div>
+			<div className="comments-wrap hide">
+				<form id="commentForm">
+					<input type="text" id="inputName" name="name" placeholder="이름" />
+					<input type="text" id="inputComment" name="comment" placeholder="내용" />
+					<button id="sendComment"></button>
+				</form>
+				{/* <div className="comment-wrap">
+					<div className="profile-image"></div>
+					<div className="name">도우너:</div>
+					<div className="comment">hello</div>
+					<div className="date"></div>
+				</div> */}
+			</div>
+		</div>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+const Header = () => {
+    return (
+        <header>
+			<div className="header-wrap">
+				<div className="title-logo"></div>
+			</div>
+		</header>
+    );
+}
+
+export default Header;
